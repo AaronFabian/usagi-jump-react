9614fa2668b2ef24cd6e5af108a13f013eec6575
Aaron Fabian <aaronfabian78@gmail.com>
2024-01-23 12:03:00 +0900

    change domain

--- src/api/deFesApi.jsx
+++ src/api/deFesApi.jsx
@@ -1,2 +1,3 @@
 // export const DE_FES_BASE_URL = 'http://127.0.0.1:3000';
-export const DE_FES_BASE_URL = 'http://172.233.85.163:3000'; // production
+// export const DE_FES_BASE_URL = 'http://172.233.85.163:3000'; // production
+export const DE_FES_BASE_URL = 'https://usagi-jump.onrender.com'; // production
