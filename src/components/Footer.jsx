f713d426fdadd18ff9cf5b806a9bfa0ab4a723ab
Aaron Fabian <aaronfabian78@gmail.com>
2024-01-26 13:27:27 +0900

    fixing img src

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,25 @@
 export default function Footer() {
 	return (
 		<footer>
-			<p className="footer_text">
-				【Fes de ジャンプ】の公式アプリを
-				<br />
-				今すぐダウンロード！
-			</p>
+			<img src="be7c132e4c8d5c897d7e3752975cda68.jpg" alt="footer image" className="footer_img_sub" />
 
-			<div className="download">
-				<img src="usagi_footer.png" alt="アプリアイコン" className="usagi_icon" />
+			<div className="footer_content_container">
+				<p className="footer_text">
+					【Fes de ジャンプ】の公式アプリを
+					<br />
+					今すぐダウンロード！
+				</p>
 
-				<img src="fesdejump.png" alt="アプリ文字" className="usagi_text" />
-			</div>
+				<div className="download">
+					<img src="usagi_footer.png" alt="アプリアイコン" className="usagi_icon" />
 
-			<div className="download_icon">
-				<img src="apple-launcher-logo.png" alt="appleダウンロード" />
-				<img src="android-launcher-logo.png" alt="androidダウンロード" />
+					<img src="fesdejump.png" alt="アプリ文字" className="usagi_text" />
+				</div>
+
+				<div className="download_icon">
+					<img src="apple-launcher-logo.png" alt="appleダウンロード" />
+					<img src="android-launcher-logo.png" alt="androidダウンロード" />
+				</div>
 			</div>
 		</footer>
 	);
